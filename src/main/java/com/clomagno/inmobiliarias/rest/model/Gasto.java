94e1b6cc53494c83cf4edfae523f39954be9ddfb
lomagno2003 <lomagno2003@gmail.com>
2014-06-25 15:20:47 -0300

    CLOSED - task 8: Solve the error warnings on model caused for the generator "gen" https://github.com/lomagno2003/inmobiliarias.rest/issues/issue/8

--- src/main/java/com/clomagno/inmobiliarias/rest/model/Gasto.java
+++ src/main/java/com/clomagno/inmobiliarias/rest/model/Gasto.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
@@ -24,7 +23,7 @@ public class Gasto implements Serializable {
 
 	@Id
 	@GenericGenerator(name="gen",strategy="increment")
-	@GeneratedValue(generator="gen")
+	@GeneratedValue(generator="gen")s
 	private long idGasto;
 	private String monto;
 	private String comprobante;
